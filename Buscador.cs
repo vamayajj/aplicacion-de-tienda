e819255e74103458570d205eeea193ae2aa8555d
Tamayovski <93049809+Tamayovski@users.noreply.github.com>
2021-10-23 11:53:05 -0500

    Script básico para el buscador.

---
+++ Buscador.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buscador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Inicialización de las variables
+
+            string producto;
+
+            Console.WriteLine("Los productos disponibles son");
+            Console.WriteLine("Paquete de galletas");
+            Console.WriteLine("Huevos");
+            Console.WriteLine("Arepas");
+            Console.WriteLine("Leche");
+            Console.WriteLine("Inserte el producto del cual desea saber el precio");
+
+            producto = Console.ReadLine();
+
+            Console.Clear();
+
+            if (producto == "Paquete de galletas")
+            {
+                Console.WriteLine("El precio del producto es" + 20000);
+                Console.ReadLine();
+            }
+
+            if (producto == "Huevos")
+            {
+                Console.WriteLine("El precio del producto es" + 23000);
+                Console.ReadLine();
+            }
+
+            if (producto == "Arepas")
+            {
+                Console.WriteLine("El precio del producto es" + 12000);
+                Console.ReadLine();
+            }
+
+            if (producto == "Leche")
+            {
+                Console.WriteLine("El precio del producto es" + 3000);
+                Console.ReadLine();
+            }
+
+            else
+            {
+                Console.WriteLine("Producto no encontrado, por favor intentelo denuevo");
+                Console.WriteLine("Los productos disponibles son");
+                Console.WriteLine("Paquete de galletas");
+                Console.WriteLine("Huevos");
+                Console.WriteLine("Arepas");
+                Console.WriteLine("Leche");
+                Console.WriteLine("Inserte el producto del cual desea saber el precio");
+                Console.ReadLine();
+            }
+
+
+        }
+
+    }
+
+}
