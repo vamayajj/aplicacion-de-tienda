0721e1065d3277a33c6776fc9779d777525fe012
Tamayovski <93049809+Tamayovski@users.noreply.github.com>
2021-10-24 19:09:30 -0500

    Script para inicio de la aplicacion

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AppTienda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Principal myPrincipal = new Principal();
+            myPrincipal.start();
+        }
+    }
+}
